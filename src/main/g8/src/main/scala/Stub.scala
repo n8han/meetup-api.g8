4856d3a47f9e6ee5a247cbc4e344891c9a8508ff
Nathan Hamblen <nathan@technically.us>
2011-01-23 11:30:15 -0500

    print the rsvp stream

--- src/main/g8/src/main/scala/Stub.scala
+++ src/main/g8/src/main/scala/Stub.scala
@@ -1,3 +1,15 @@
-class Stub {
-  // got any helpful boilerplate for your users?
+package com.example
+
+import dispatch._
+import dispatch.meetup._
+import dispatch.liftjson.Js._
+
+object MyApplication {
+  def main(args: Array[String]) {
+    val h = new nio.Http
+    h(RsvpsStream.open(None) { js => 
+      println(prettyrender(js))
+      println("press ctrl+c to quit")
+    })
+  }
 }
